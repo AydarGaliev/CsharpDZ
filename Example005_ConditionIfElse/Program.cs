357b1e24414cec2231e58d28c30fe017b912245b
Aydar <3ajimka@gmail.com>
2022-04-12 23:31:39 +0300

    add example 005 ondition if-else project

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "айдар")
+{
+    Console.WriteLine("Ура, это же Айдар!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
